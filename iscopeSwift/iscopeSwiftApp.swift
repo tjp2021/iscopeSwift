71690a12446b5e9f15ab220319503a800e0561bf
tyjoo <tyjoo21@gmail.com>
2025-02-06 20:49:54 -0600

    Initial Commit

---
+++ iscopeSwift/iscopeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  iscopeSwiftApp.swift
+//  iscopeSwift
+//
+//  Created by Timothy Joo on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iscopeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
